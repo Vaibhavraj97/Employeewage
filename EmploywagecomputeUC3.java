ea66a71f179b68cdeee0abb7cd5e4b07e418273a
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-01-27 10:20:44 +0530

    Refactored code adding part time employwage value

---
+++ EmploywagecomputeUC3.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.employwagebuilder;
+
+public class EmploywagecomputeUC3 {
+	int IS_PART_TIME=1;
+	  int IS_FULL_TIME =2;
+	  int EMP_RATE_PER_HOUR=20;
+
+	public void empWage(){
+	  int emphrs=0;
+	  int empwage=0;
+
+	  double empcheck = Math.floor(Math.random() * 10) % 3;
+
+	  if (empcheck == IS_FULL_TIME)
+	  {  emphrs=8;
+	  
+	  }
+	  else if (empcheck == IS_PART_TIME)
+	  { emphrs=4;
+	  
+	  }
+
+	     empwage=emphrs * EMP_RATE_PER_HOUR;
+
+	     System.out.println("EMPWAGE="+empwage);
+	    }
+	  public static void main(String [] args){
+	    EmploywagecomputeUC3 e=new EmploywagecomputeUC3();
+	    e.empWage();
+	   }
+	 }
+	
+
