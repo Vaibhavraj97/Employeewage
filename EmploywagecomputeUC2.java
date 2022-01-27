615dc4f9c78a2c6fee92970760c07055ca879070
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-01-27 10:16:04 +0530

    Refactored code assuming values of wage/hr fulldayhr

---
+++ EmploywagecomputeUC2.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.employwagebuilder;
+
+public class EmploywagecomputeUC2 {
+	int IS_FULL_TIME=1;
+	   int EMP_RATE_PER_HOUR=20;
+
+	   public void empWage(){
+	      int emphrs=0;
+	      int empwage=0;
+
+	      double empcheck = Math.floor(Math.random() * 10) % 2;
+
+	      if (empcheck == IS_FULL_TIME){
+	        emphrs=8;
+	      }
+
+	      empwage=emphrs * EMP_RATE_PER_HOUR;
+
+	      System.out.println("EMPWAGE="+empwage);
+	     }
+
+	    public static void main(String [] args){
+	      EmploywagecomputeUC2 e=new EmploywagecomputeUC2();
+	      e.empWage();
+	     }
+	   }
+
+
